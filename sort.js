fc2ad64434f971cbda17e133de0af565bcaeb3f1
James Browning <thejamesernator@gmail.com>
2020-10-04 13:44:15 +1300

    .

--- sort.js
+++ sort.js
@@ -1,2 +1,24 @@
 import fs from 'fs/promises';
 import yaml from 'js-yaml';
+
+function by(toValue) {
+    return function(a, b) {
+        const valA = toValue(a);
+        const valB = toValue(b);
+        if (valA < valB) {
+            return -1;
+        } else if (valA === valB) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
+
+const FILE = new URL('./origami-design-index.yaml', import.meta.url);
+
+const contents = await fs.readFile(FILE, 'utf8');
+const items = yaml.load(contents);
+items.sort(by(i => i.title));
+const sortedContents = yaml.dump(contents);
+await fs.writeFile(FILE, sortedContents);
